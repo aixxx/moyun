40f2dbd87f863238350ea4bdb4678f2b9f50494d
xuxiao <aix_xx@yeah.net>
2018-07-23 13:55:22 +0800

    1

--- application/common.php
+++ application/common.php
@@ -387,15 +387,14 @@ if (!function_exists('getBrowseType')) {
 
 if (!function_exists('curl_get_https')) {
     function curl_get_https($url){
-        $curl = curl_init(); // 启动一个CURL会话
+
+        $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_HEADER, 1);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); // 跳过证书检查
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);  // 从证书中检查SSL加密算法是否存在
-        $tmpInfo = curl_exec($curl);     //返回api的json对象
-        //关闭URL请求
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);//这个是重点。
+        $data = curl_exec($curl);
         curl_close($curl);
-        return $tmpInfo;    //返回json对象
+
     }
 }
