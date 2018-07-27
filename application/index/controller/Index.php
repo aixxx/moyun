b55a5d4f42d9d69b65e11ebd3064c0a113816fb9
xuxiao <aix_xx@yeah.net>
2018-07-27 10:20:00 +0800

    11

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -19,15 +19,16 @@ class Index extends Frontend
      */
     public function index()
     {
+        $getbyid = Request::instance()->param("getbyid",0,"intval");
         //session存在，跳转活动首页
         if(session("MOBOO_OAUTH_ID")) {
             $isUpload = action("Api/getIsUpload");
-            $params = ['id'=> session("MOBOO_OAUTH_ID"),'is_upload'=> $isUpload];
+            $params = ['id'=> session("MOBOO_OAUTH_ID"),'is_upload'=> $isUpload,'getbyid'=> $getbyid];
             $this->redirect($this->activityHome ."?". http_build_query($params),302);
         }
         $oauthUrl = Env::get('oauth.oauthUrl') . "/sso/auth";
         $oauthUrl .= "?appkey=".Env::get('oauth.AppKey');
-        $oauthUrl .= "&successurl=".urlencode(Env::get('oauth.successurl'));
+        $oauthUrl .= "&successurl=".urlencode(Env::get('oauth.successurl')."?getbyid=".$getbyid);
         $this->redirect($oauthUrl,302);
     }
 
@@ -36,6 +37,7 @@ class Index extends Frontend
      * */
     public function callback(){
         $token = Request::instance()->param("token","");
+        $getbyid = Request::instance()->param("getbyid",0,"intval");
         if(!$token) jsond(0, 'token not found');
         //https://devauth.gomoboo.com/sso/validate?token=498381557-83362202-af93-48d1-ab6eaa19b6c95c44&appkey=xxx&appsecret=xyz
         $url = Env::get('oauth.oauthUrl') . "/sso/validate";
@@ -71,7 +73,7 @@ class Index extends Frontend
             $isUpload = 1;
         }
         //记录成功，跳转活动首页
-        $params = ['id'=> session("MOBOO_OAUTH_ID"),'is_upload'=> $isUpload];
+        $params = ['id'=> session("MOBOO_OAUTH_ID"),'is_upload'=> $isUpload, 'getbyid'=> $getbyid];
         $this->redirect($this->activityHome ."?". http_build_query($params),302);
     }
 }
