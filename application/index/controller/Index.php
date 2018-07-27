5f0b384829b6e7f73df10c38e5f599d2ea78f6eb
xuxiao <aix_xx@yeah.net>
2018-07-27 18:58:24 +0800

    123

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -64,7 +64,6 @@ class Index extends Frontend
             'profile_desc' => $result["profileDesc"],
             'platform' => getBrowseType() ?: 'mobu',
         ];
-        print_r($data);die;
         if($info){
             $is_save = $oauth->isUpdate(true)->save($data, ["id"=> $info["id"]]);
             session("MOBOO_OAUTH_ID", $info["id"]);
