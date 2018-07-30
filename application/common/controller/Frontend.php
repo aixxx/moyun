6b84b3c0730336bfcce3c4d25065aa6b9d5c963c
xuxiao <aix_xx@yeah.net>
2018-07-30 15:17:22 +0800

    1

--- application/common/controller/Frontend.php
+++ application/common/controller/Frontend.php
@@ -18,7 +18,7 @@ class Frontend extends Controller
     public function _initialize()
     {
         $this->browseType = getBrowseType();
-        if(!$this->browseType && ACTION_NAME != 'getshare') $this->error("目前只支持微博，微信，QQ，魔部APP 里打开，敬请谅解!");
+        if(!$this->browseType) $this->error("目前只支持微博，微信，QQ，魔部APP 里打开，敬请谅解!");
         $this->activityHome = Env::get('oauth.activityHome');
     }
 
--- application/index/controller/Api.php
+++ application/index/controller/Api.php
@@ -326,7 +326,9 @@ class Api extends Frontend
     public function getShare(){
         $url = Env::get("oauthUrl")."/3rd/weixin/config";
         $url .= "?url=".  urlencode($_SERVER['HTTP_REFERER']);
+        echo $_SERVER['HTTP_REFERER']."<br />";
         $res = curl_get_https($url);
+        echo $res;die;
         $res = json_decode($res, true);
         echo json_encode($res['result']);
         die;
